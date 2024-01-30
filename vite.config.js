46bf2cc5af203647a9c0412cc208f4c24655f905
jorgelnc <jorgenavacolina@gmail.com>
2024-01-30 10:18:13 -0500

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -6,16 +6,12 @@ import glob from 'glob';
 export default defineConfig({
   build: {
     lib: {
-      entry: glob.sync(path.resolve(__dirname, 'lib/**/*.js')),
+      entry: glob.sync(path.resolve(__dirname, "src/**/*.{js,css,jsx}")),
       name: '',
     },
     rollupOptions: {
       output: {
         preserveModules: true,
-        preserveModulesRoot: "src",
-        entryFileNames: ({ name: fileName }) => {
-          return `${fileName}.js`
-        },
       },
     },
   },
