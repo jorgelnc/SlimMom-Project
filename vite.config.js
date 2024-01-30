02caa3ca92037a01365d92ef5e37774b8c416ebb
jorgelnc <jorgenavacolina@gmail.com>
2024-01-30 10:31:36 -0500

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,46 +1,24 @@
+import path from "path";
+import glob from "glob";
 
-import { defineConfig } from 'vite';
-import path from 'path';
-import glob from 'glob';
-
-export default defineConfig({
-  build: {
-    lib: {
-      entry: glob.sync(path.resolve(__dirname, "lib/**/*.{js,css,jsx}")),
-      name: '',
-    },
-    rollupOptions: {
-      output: {
-        preserveModules: true,
-      },
-    },
-  },
-},);
-
-
-
-  
-// import path from "path";
-// import glob from "glob";
-
-// export default {
-//     build: {
-//         outDir: path.resolve(__dirname, "build"),
-//         emptyOutDir: true,
-//         cssCodeSplit: true,
-//         lib: {
-//             entry: path.resolve(__dirname, "src/**/*.{js,css,jsx}"),
-//             formats: ["esm"]
-//         },
-//         rollupOptions: {
-//             input: glob.sync(path.resolve(__dirname, "src/**/*.{js,css,jsx}")),
-//             output: {
-//                 preserveModules: true,
-//                 preserveModulesRoot: "src",
-//                 entryFileNames: ({name: fileName}) => {
-//                     return `${fileName}.js`
-//                 }
-//             }
-//         }
-//     }
-// }
+export default {
+    build: {
+        outDir: path.resolve(__dirname, "build"),
+        emptyOutDir: true,
+        cssCodeSplit: true,
+        lib: {
+            entry: path.resolve(__dirname, "src/**/*.{js,css,jsx}"),
+            formats: ["esm"]
+        },
+        rollupOptions: {
+            input: glob.sync(path.resolve(__dirname, "src/**/*.{js,css,jsx}")),
+            output: {
+                preserveModules: true,
+                preserveModulesRoot: "src",
+                entryFileNames: ({name: fileName}) => {
+                    return `${fileName}.js`
+                }
+            }
+        }
+    }
+}
