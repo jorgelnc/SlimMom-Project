62dcfc66e4441cf2514fbe8d2f897b4d7c5b9f1a
jorgelnc <jorgenavacolina@gmail.com>
2024-01-30 14:39:08 -0500

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig(({ command }) => {
       [command === 'serve' ? 'global' : '_global']: {},
     },
     root: 'src',
-    plugins: [ReactRefresh(), injectHTML(), FullReload(['./src/**/**.{js,jsx,html}'])],
+    plugins: [ReactRefresh(), injectHTML(), FullReload(['./src/**/**.js'])],
     build: {
       sourcemap: true,
       outDir: '../dist',
