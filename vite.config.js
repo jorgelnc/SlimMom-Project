a48e519baa98c428cef6f3d7d8eb481e3509d49a
jorgelnc <jorgenavacolina@gmail.com>
2024-01-30 14:43:26 -0500

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,58 +1,25 @@
-import { defineConfig } from 'vite';
+import path from "path";
+import glob from "glob";
 
-export default defineConfig(({ command }) => {
-  return {
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
-    },
-    root: 'src',
+export default {
     build: {
-      sourcemap: true,
-      outDir: '../dist',
-      jsxInject: `import React from 'react'`,
-      rollupOptions: {
-        input: {
-          main: './src/**/**.{js,jsx,html}',
+        outDir: path.resolve(__dirname, "build"),
+        emptyOutDir: true,
+        cssCodeSplit: true,
+        jsxInject: `import React from 'react'`,
+        lib: {
+            entry: path.resolve(__dirname, "src/index.jsx"),
+            formats: ["esm"]
         },
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
+        rollupOptions: {
+            input: glob.sync(path.resolve(__dirname, "src/index.jsx")),
+            output: {
+                preserveModules: true,
+                preserveModulesRoot: "src",
+                entryFileNames: ({name: index}) => {
+                    return `${index}.js`
+                }
             }
-          },
-          entryFileNames: 'commonHelpers.js',
-        },
-      },
-    },
-    optimizeDeps: {
-      include: ['react', 'react-dom'], // Añade otras dependencias si las necesitas
-    },
-  };
-});
-
-
-// import path from "path";
-// import glob from "glob";
-
-// export default {
-//     build: {
-//         outDir: path.resolve(__dirname, "build"),
-//         emptyOutDir: true,
-//         cssCodeSplit: true,
-//         jsxInject: `import React from 'react'`,
-//         lib: {
-//             entry: path.resolve(__dirname, "src/index.jsx"),
-//             formats: ["esm"]
-//         },
-//         rollupOptions: {
-//             input: glob.sync(path.resolve(__dirname, "src/index.jsx")),
-//             output: {
-//                 preserveModules: true,
-//                 preserveModulesRoot: "src",
-//                 entryFileNames: ({name: index}) => {
-//                     return `${index}.jsx`
-//                 }
-//             }
-//         }
-//     }
-// }
+        }
+    }
+}
