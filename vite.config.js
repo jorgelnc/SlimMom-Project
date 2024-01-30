a7545ca91ce70e2367d0d82e301caeb5a369acc8
jorgelnc <jorgenavacolina@gmail.com>
2024-01-30 08:59:36 -0500

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import { resolve } from 'path';
+import glob from "glob";
 
 export default defineConfig(({ command }) => {
   return {
