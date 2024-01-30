535bc02312ce4b35b0086504f7ea068993e057a2
jorgelnc <91018922+jorgelnc@users.noreply.github.com>
2024-01-30 19:29:56 +0000

    Deploying to gh-pages from @ jorgelnc/SlimMom-Project@f888705016370406ca568d791dcba663a9022386 🚀

--- pages/DiaryPage.js
+++
@@ -1,7 +0,0 @@
-import e from "../node_modules/react/index.js";
-import r from "../components/Header/Header.js";
-import t from "../components/DiaryTable/DiaryTable.js";
-const n = () => /* @__PURE__ */ e.createElement("div", null, /* @__PURE__ */ e.createElement(r, { currentPage: "Diary" }), /* @__PURE__ */ e.createElement(t, null));
-export {
-  n as default
-};
---
+++ pages/RegisterPage.jsx
@@ -0,0 +1,7 @@
+import e from "../node_modules/react/index.jsx";
+import r from "../components/Header/Header.jsx";
+import t from "../components/Auth/RegisterForm.jsx";
+const l = () => /* @__PURE__ */ e.createElement("div", null, /* @__PURE__ */ e.createElement(r, { currentPage: "Register" }), /* @__PURE__ */ e.createElement(t, null));
+export {
+  l as default
+};
