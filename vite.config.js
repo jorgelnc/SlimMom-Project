fecdfdabe7cf99422b246a8fbca90db3e3d505ae
jorgelnc <jorgenavacolina@gmail.com>
2024-01-30 14:10:54 -0500

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -7,16 +7,16 @@ export default {
         emptyOutDir: true,
         cssCodeSplit: true,
         lib: {
-            entry: path.resolve(__dirname, "src/**/*.{js,css,jsx, html}"),
+            entry: path.resolve(__dirname, "src/.{js,css,jsx, html}"),
             formats: ["esm"]
         },
         rollupOptions: {
-            input: glob.sync(path.resolve(__dirname, "src/**/*.{js,css,jsx, html}")),
+            input: glob.sync(path.resolve(__dirname, "src/.{js,css,jsx, html}")),
             output: {
                 preserveModules: true,
                 preserveModulesRoot: "src",
                 entryFileNames: ({name: index}) => {
-                    return `${index}.js`
+                    return `${index}.jsx`
                 }
             }
         }
