e9e166cf8b104e04da0f38ba81339bece6eb5707
jorgelnc <jorgenavacolina@gmail.com>
2024-02-01 18:42:33 -0500

    updated files repo

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,20 @@ import path from 'path';
 import glob from 'glob';
 
 export default {
-  esbuild: {
+  build: {
     outDir: path.resolve(__dirname, 'build'),
     emptyOutDir: true,
     cssCodeSplit: true,
     jsxInject: `import React from 'react'`,
     lib: {
-      entry: path.resolve(__dirname, 'src/index.{js,html}'),
+      entry: path.resolve(__dirname, 'src/index.{js,html,jsx}'),
       formats: ['esm'],
     },
     rollupOptions: {
-      input: glob.sync(path.resolve(__dirname, 'src/index.{js,html}')),
+      input: glob.sync(path.resolve(__dirname, 'src/index.{js,html,jsx}')),
       output: {
         preserveModules: true,
-        preserveModulesRoot: 'src/index.{js,html}',
+        preserveModulesRoot: 'src/index.{js,html,jsx}',
         entryFileNames: ({ name: index }) => {
           return `${index}.js`;
         },
