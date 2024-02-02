54bb3ed7ae8a3bdb3ba6a41f1e44f27bb1288baa
jorgelnc <jorgenavacolina@gmail.com>
2024-02-01 19:04:46 -0500

    updated files repo

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,41 @@
-import path from 'path';
-import glob from 'glob';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default {
-  build: {
-    outDir: path.resolve(__dirname, 'build'),
-    emptyOutDir: true,
-    cssCodeSplit: true,
-    jsxInject: `import React from 'react'`,
-    lib: {
-      entry: path.resolve(__dirname, 'src/index.{js,html,jsx}'),
-      formats: ['esm'],
-    },
-    rollupOptions: {
-      input: glob.sync(path.resolve(__dirname, 'src/index.{js,html,jsx}')),
-      output: {
-        preserveModules: true,
-        preserveModulesRoot: 'src/index.{js,html,jsx}',
-        entryFileNames: ({ name: index }) => {
-          return `${index}.js`;
-        },
-      },
+export default defineConfig({
+  plugins: [react()],
+  esbuild: {
+    jsxFactory: 'React.createElement',
+    jsxFragment: 'React.Fragment',
+  },
+  css: {
+    modules: {
+      localsConvention: 'camelCaseOnly',
     },
   },
-};
+});
+
+// import path from 'path';
+// import glob from 'glob';
+
+// export default {
+//   build: {
+//     outDir: path.resolve(__dirname, 'build'),
+//     emptyOutDir: true,
+//     cssCodeSplit: true,
+//     jsxInject: `import React from 'react'`,
+//     lib: {
+//       entry: path.resolve(__dirname, 'src/index.{js,html,jsx}'),
+//       formats: ['esm'],
+//     },
+//     rollupOptions: {
+//       input: glob.sync(path.resolve(__dirname, 'src/index.{js,html,jsx}')),
+//       output: {
+//         preserveModules: true,
+//         preserveModulesRoot: 'src/index.{js,html,jsx}',
+//         entryFileNames: ({ name: index }) => {
+//           return `${index}.js`;
+//         },
+//       },
+//     },
+//   },
+// };
