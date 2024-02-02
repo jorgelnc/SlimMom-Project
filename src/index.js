7ae048b98313bdd3001f08af1f2025c1043aa738
jorgelnc <jorgenavacolina@gmail.com>
2024-02-01 19:56:19 -0500

    updated files repo

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client'; // Asegúrate de que la ruta sea correcta
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App.jsx'; // Asegúrate de que la ruta sea correcta
 
-const root = createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 // Renderiza tu aplicación usando root.render
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
