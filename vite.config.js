66075d3fd6b1024d96aa3908d065a61369b78570
jorgelnc <jorgenavacolina@gmail.com>
2024-02-01 19:08:48 -0500

    updated files repo

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,35 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import glob from 'glob';
 
 export default defineConfig({
   plugins: [react()],
   esbuild: {
     jsxFactory: 'React.createElement',
     jsxFragment: 'React.Fragment',
+    jsxInject: `import React from 'react'`,
   },
-  css: {
-    modules: {
-      localsConvention: 'camelCaseOnly',
+  lib: {
+    entry: path.resolve(__dirname, 'src/index.{js,html,jsx}'),
+    formats: ['esm'],
+  },
+  rollupOptions: {
+    input: glob.sync(path.resolve(__dirname, 'src/index.{js,html,jsx}')),
+    output: {
+      preserveModules: true,
+      preserveModulesRoot: 'src/index.{js,html,jsx}',
+      entryFileNames: ({ name: index }) => {
+        return `${index}.js`;
+      },
+    },
+    css: {
+      modules: {
+        localsConvention: 'camelCaseOnly',
+      },
     },
   },
 });
-
-// import path from 'path';
-// import glob from 'glob';
 
 // export default {
 //   build: {
